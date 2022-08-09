b77bbae545437e77b63c8fc0053e60fb90fa994e
Huo Linhe <linhehuo@gmail.com>
2022-08-09 14:38:51 +0800

    chore(py): use taosws as driver name in python

--- taos-ws-py/examples/pandas-read-sql.py
+++ taos-ws-py/examples/pandas-read-sql.py
@@ -2,6 +2,6 @@ import pandas
 from sqlalchemy import create_engine
 import taosws
 
-engine = taosws.connect("taos://localhost:6041")
+engine = taosws.connect("taosws://localhost:6041")
 res = pandas.read_sql("show databases", engine)
 print(res)
